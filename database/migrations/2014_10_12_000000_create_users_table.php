6b45e1b04154473c1f0100e1e81b81f2f08f5be8
vithosengka <sengkavito@gmail.com>
2022-05-16 06:44:46 +0700

    membuat migration semua table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,27 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')
+                  ->unique();
+            $table->timestamp('email_verified_at')
+                  ->nullable();
             $table->string('password');
+            $table->string('phone');
+            $table->enum('gender', ['laki-laki', 'perempuan'])
+                  ->nullable();
+            $table->date('birt_date')
+                  ->nullable();
+            $table->string('job')
+                  ->nullable();
+            $table->text('address')
+                  ->nullable();
+            $table->text('about')
+                  ->nullable();
+            $table->string('path_image') ->nullable();
+            $table->bigInteger('role_id');
             $table->rememberToken();
             $table->timestamps();
+
         });
     }
 
